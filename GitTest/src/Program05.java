1f8cd1f973f169fbc185c07eebedc760fb381389
eiichiro_arakawa <itlabo.arakawa@gmail.com>
2021-04-03 16:48:28 +0900

    「Program05」年齢変更

--- GitTest/src/Program05.java
+++ GitTest/src/Program05.java
@@ -3,7 +3,7 @@ public class Program05 {
 	public static void main(String[] args) {
 		System.out.println("北島洋子");
 		System.out.println("女性");
-		System.out.println(30);
+		System.out.println(31);
 	}
 
 }
